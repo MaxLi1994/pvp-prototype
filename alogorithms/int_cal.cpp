b5011a579f7f048bfc768ae85bed7fa312d146e0
Max <415899517@qq.com>
2018-02-01 22:53:26 -0500

    Update int_cal.cpp

--- alogorithms/int_cal.cpp
+++ alogorithms/int_cal.cpp
@@ -2,26 +2,10 @@
 
 using namespace pvp;
 
-DataBucket add(DataBucket data, Params params)
+extern "C" DataBucket& add(DataBucket &data, Params &params)
 {
     int a = data.get_int("a");
     int b = data.get_int("b");
     data.set_int("result", a+b);
-    return data;
-}
-
-DataBucket sub(DataBucket data, Params params)
-{
-    int a = data.get_int("a");
-    int b = data.get_int("b");
-    data.set_int("result", a-b);
-    return data;
-}
-
-DataBucket mul(DataBucket data, Params params)
-{
-    int a = data.get_int("a");
-    int b = data.get_int("b");
-    data.set_int("result", a*b);
     return data;
 }
